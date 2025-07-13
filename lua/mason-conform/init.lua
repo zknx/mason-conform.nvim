bdc8c51e3f2321e6abcec5640b50a3a4ff5d8641
zknx <zknx.dev@gmail.com>
2025-07-14 04:16:46 +0900

    docs(config): add type declarations for explicit configuration schema documentation

--- lua/mason-conform/init.lua
+++ lua/mason-conform/init.lua
@@ -1,10 +1,15 @@
 local M = {}
 
+--- @class (exact) mason-conform.setupOpts
+--- @field ignore_install? string[]
+--- @field ensure_install? string[]
+--- @type mason-conform.setupOpts
 M.config = {
     ignore_install = {},
     ensure_install = {},
 }
 
+--- @param cfg? mason-conform.setupOpts
 function M.setup(cfg)
     if cfg then
         if cfg.ignore_install then
